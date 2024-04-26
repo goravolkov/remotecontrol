a23080315528033dc9c7587c6f644c51f147f25f
goravolkov <goravolkoff@gmail.com>
2024-04-26 19:35:14 +0300

    Removed user info variable

---
+++ utility_lvgw_256022_8nl3.tsx
@@ -0,0 +1,3 @@
+const findSmallestNumber = numbers => Math.min(...numbers);
+const removeDuplicates = array => Array.from(new Set(array));
+const shuffleArray = array => array.sort(() => Math.random() - 0.5);
